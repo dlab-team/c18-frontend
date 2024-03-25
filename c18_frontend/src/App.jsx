8cf2239aa8683ff4e2fa42db098c9b428f8ded86
Luis O <30819865+hlcxpl@users.noreply.github.com>
2024-03-25 15:13:25 -0300

    fix: arreglando primero commit

--- c18_frontend/src/App.jsx
+++ c18_frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <h1 className="text-4xl font-bold underline bg-black text-white ">Hello world!</h1>;
+  return <h1 className="text-4xl font-bold underline bg-black text-white text-wrap top-3 ">Hello world!</h1>;
 }
 
 export default App;
